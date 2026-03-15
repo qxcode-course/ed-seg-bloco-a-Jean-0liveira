abc22a28f5e0f3e1d728eddd09b7ddd404f3821e
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-03-15 18:31:24 +0000

    Atividade pitando a casa, feita!

--- ed/pintando/go/draft.go
+++ ed/pintando/go/draft.go
@@ -1,5 +1,21 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-    fmt.Println("qxcode")
+	var a, b, c float64
+
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&c)
+
+	p := (a + b + c) / 2
+
+	area := math.Sqrt(p * (p - a) * (p - b) * (p - c))
+
+	fmt.Printf("%.2f\n", area)
+
 }

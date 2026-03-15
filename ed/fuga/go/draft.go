cf45d8d5237b623dff8534730725f69bfcc0b662
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-03-15 20:01:27 +0000

    Atividade fuga, feita!

--- ed/fuga/go/draft.go
+++ ed/fuga/go/draft.go
@@ -1,5 +1,25 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+
+	var h, p, f, d int
+
+	fmt.Scan(&h)
+	fmt.Scan(&p)
+	fmt.Scan(&f)
+	fmt.Scan(&d)
+
+	for {
+		f = (f + d + 16) % 16
+
+		if f == h {
+			fmt.Println("S")
+			break
+		} else if f == p {
+			fmt.Println("N")
+			break
+		}
+	}
 }

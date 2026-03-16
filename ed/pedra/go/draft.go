33a9dce6ea851b2dcc37551cd5ab855caf4e0f9f
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-03-16 19:59:32 +0000

    Fazendo a atividade, pedra

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -4,10 +4,23 @@ import "fmt"
 
 func main() {
     
-    var a, b string
+    var n int
 
-    fmt.Scan(&a)
-    fmt.Scan(&b)
+    fmt.Scan(&n)
 
-    
+    a := make([] int, n)
+    b := make([] int, n)
+
+    for i := 0; i < n; i++ {
+        fmt.Scan(&a[i], &b[i])
+    }
+
+    menorValor := 101
+    vencedor := -1
+
+    for i := 0; i < n; i++ {
+        a := a[i]
+        b := b[i]
+    }
+
 }

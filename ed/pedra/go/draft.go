f32cb6a76153cb344922af2892316ac4c352c064
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-03-16 21:58:28 +0000

    Finalizando atividade, pedra.

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "math"
 
 func main() {
     
@@ -8,19 +9,35 @@ func main() {
 
     fmt.Scan(&n)
 
-    a := make([] int, n)
-    b := make([] int, n)
+    pa := make([] int, n)
+    pb := make([] int, n)
 
     for i := 0; i < n; i++ {
-        fmt.Scan(&a[i], &b[i])
+        fmt.Scan(&pa[i], &pb[i])
     }
 
     menorValor := 101
     vencedor := -1
 
     for i := 0; i < n; i++ {
-        a := a[i]
-        b := b[i]
-    }
+        
+        a := pa[i]
+        b := pb[i]
 
+        if a < 10 || b < 10{
+            continue
+        }
+
+        dif := int(math.Abs(float64(a - b)))
+
+        if dif < menorValor {
+            menorValor = dif
+            vencedor = i
+        }
+    }
+    if vencedor == -1 {
+        fmt.Println("sem ganhador")
+    } else {
+        fmt.Println(vencedor)
+    }
 }

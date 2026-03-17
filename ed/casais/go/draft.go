d6e459d1063c0414848719ce21c440f052424a53
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-03-17 19:55:09 +0000

    Fazendo a atividade, casais

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -6,11 +6,22 @@ import (
 
 func main() {
     
-    var n int 
+    var n int
 
     fmt.Scan(&n)
 
-    
+    macho := make([]int, 0)
+    femea := make([] int, 0)
+
+    for i := 0; i < n; i++ {
+        fmt.Scan(&n)
+
+        if n > 0 {
+            macho = append(macho, n)
+        } else {
+            femea = append(femea, n)
+        }
+    }
 
     fmt.Println(n)
 }

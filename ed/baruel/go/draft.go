c2d127b44f772e7927ba83baddfddf71f03e3a95
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-03-21 20:23:40 +0000

    Fazendo atividade, baruel

--- ed/baruel/go/draft.go
+++ ed/baruel/go/draft.go
@@ -5,22 +5,14 @@ import "fmt"
 func main() {
     var qtd_fig_album int
     var qtd_fig_baruel int 
-    qtd_repetidas := 0
 
     fmt.Scan(&qtd_fig_album)
     fmt.Scan(&qtd_fig_baruel)
 
-    repetidas := make([] int, 0)
+    fig := make([] int, qtd_fig_baruel)
     
     for i := 0; i < qtd_fig_baruel; i++ {
-        var x int
-        fmt.Scan(&x)
-
-        if x == 
-        repetidas = append(repetidas, x)
-
-        
-
+        fmt.Scan(&fig[i])
     }
-
+    fmt.Println(fig)
 }

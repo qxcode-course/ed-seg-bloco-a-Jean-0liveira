91dc39c3844afd19d4156846782dcc5ad56b48a9
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-03-21 02:24:12 +0000

    Coomeçando atividade, baruel

---
+++ ed/baruel/go/draft.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    var qtd_fig_album int
+    var qtd_fig_baruel int 
+    var sequencia int 
+
+    fmt.Scan(&qtd_fig_album)
+    fmt.Scan(&qtd_fig_baruel)
+    fmt.Scan(&sequencia)
+
+    fmt.Println(qtd_fig_album)
+    fmt.Println(qtd_fig_baruel)
+    fmt.Println(sequencia)
+
+}

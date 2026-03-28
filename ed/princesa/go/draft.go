0f87a192d5fe700176f1cef6dcaf81c24d6a0853
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-03-28 22:31:55 +0000

    Fazendo aividade, princesa

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,5 +1,28 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+)
+
 func main() {
-    fmt.Println("qxcode")
+    var n, e int
+
+    fmt.Scan(&n)
+    fmt.Scan(&e)
+
+    fila := make([] int, n)
+
+    for i := 0; i < n; i++{
+        fila[i] = i + 1
+    }
+
+    fmt.Print("[ ")
+    for _, p := range fila {
+        if p == e {
+            fmt.Print(p, "> ")
+        } else {
+            fmt.Print(p, " ")
+        }
+    }
+    fmt.Print("]")
 }

e5028573a8dbcd67245704f77e6b860b928a8d17
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-03-29 16:20:08 +0000

    Fazendo atividade, princesa

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -10,19 +10,46 @@ func main() {
     fmt.Scan(&n)
     fmt.Scan(&e)
 
-    fila := make([] int, n)
+    vivos := make([] bool, n)
 
-    for i := 0; i < n; i++{
-        fila[i] = i + 1
+    for v := range vivos{
+        vivos[v] = true
     }
 
-    fmt.Print("[ ")
-    for _, p := range fila {
-        if p == e {
-            fmt.Print(p, "> ")
-        } else {
-            fmt.Print(p, " ")
+    pos := e - 1
+    num_vivos := n
+
+    for {
+        fmt.Print("[ ")
+
+        for i := 0; i < len(vivos); i++{
+
+            if vivos[i] {
+                if i == pos {
+                    fmt.Print(i+1, "> ")
+                } else {
+                    fmt.Print(i+1, " ")
+                }
+            }
         }
+
+        fmt.Println("]")
+
+        if num_vivos == 1{
+            break
+        }
+
+        matar := pos
+
+        for {
+            matar := (matar + 1) % num_vivos
+
+            if vivos[matar]{
+                break
+            }
+        }
+    
+        vivos[matar] = false
+        num_vivos--
     }
-    fmt.Print("]")
 }

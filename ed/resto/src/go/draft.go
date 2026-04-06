4b35f369fdda8ae4bf0a3fe3f20a03ff5a3226d5
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-04-06 18:30:30 +0000

    Iniciando atividade, resto

---
+++ ed/resto/src/go/draft.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    var num int
+
+    fmt.Scan(&num)
+
+    for 
+
+    divisao := num / 2
+    resto := num % 2
+
+    fmt.Println(divisao, resto)
+}

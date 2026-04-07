b3867314237c2871c4975c11fcc68e6ea67d831b
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-04-07 19:58:44 +0000

    Fazendo fractais.png

--- ed/fractais_png/src/go/main.go
+++ ed/fractais_png/src/go/main.go
@@ -15,6 +15,13 @@ func main() {
 	pen.SetPosition(250, 500) // move o pincel para x 250, y 500
 	pen.SetHeading(90)        // coloca o pincel apontando para cima
 	pen.Walk(200)             // anda 100 pixels
+	pen.Left(30)
+	pen.Walk(100)
+	pen.Down()
+	pen.Walk(100)
+	pen.Up()
+	pen.Right(30)
+	pen.Walk(100)
 	for range 10 {
 		pen.Up()
 		pen.Walk(30) // anda sem riscar

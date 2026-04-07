2f889d70df0b11bfaf4be68d63936ed65bb40b8a
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-04-07 18:12:22 +0000

    Finalizando atividade, resto

--- ed/resto/src/go/draft.go
+++ ed/resto/src/go/draft.go
@@ -2,15 +2,23 @@ package main
 
 import "fmt"
 
+func divisao(n int){
+    if n == 0{
+        return
+    }
+
+    prox_num := n / 2
+    resto := n % 2
+
+    divisao(prox_num)
+
+    fmt.Println(prox_num, resto)
+}
+
 func main() {
     var num int
 
     fmt.Scan(&num)
 
-    for 
-
-    divisao := num / 2
-    resto := num % 2
-
-    fmt.Println(divisao, resto)
+    divisao(num)
 }

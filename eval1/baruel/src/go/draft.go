65d7e5a4dca148908022da9b61508bf9b55f8f48
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-04-14 00:22:00 +0000

    Fazendo teste 1

--- eval1/baruel/src/go/draft.go
+++ eval1/baruel/src/go/draft.go
@@ -15,11 +15,49 @@ func main() {
         fmt.Scan(&figs[i])
     }
 
-    fmt.Print(figs)
-
     reps := make(map[int]int)
 
     for _, f := range figs{
         reps[f]++
+    }
+     
+    pri_rep := true
+    repetiu := false
+
+    for _, f := range figs{
+        if reps[f] > 1{
+            if !pri_rep{
+                fmt.Print(" ")
+            }
+            fmt.Print(f)
+            repetiu = true
+            pri_rep = false
+            reps[f]--
+        }
+    }
+
+    if !repetiu {
+        fmt.Println("N")
+    } else {
+        fmt.Println()
+    }
+
+    pri_fal := true
+    faltou := false
+
+    for i := 1; i <= qtd_total_fig; i++{
+        if reps[i] == 0{
+            if !pri_fal{
+                fmt.Print(" ")
+            }
+            fmt.Print(i)
+            pri_fal = false
+            faltou = true
+        }
+    }
+    if faltou{
+        fmt.Println()
+    } else {
+        fmt.Println("N")
     }
 }

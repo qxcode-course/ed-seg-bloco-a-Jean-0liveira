fe3f4710b5955125f140db7f6fe056b7a5697fef
Jean Oliveira <jean.oliveira2128@gmail.com>
2026-06-01 01:24:38 +0000

    Fazendo setbuild

---
+++ ed/setbuild/src/go/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Set struct {
+	data     []int
+	size     int
+	capacity int
+}
+
+func NewSet(capacity int) *Set {
+	return &Set{
+		data:     make([]int, capacity),
+		size:     0,
+		capacity: capacity,
+	}
+}
+
+func (s *Set) Show() string {
+	if s.size == 0 {
+		return "[]"
+	}
+	var result strings.Builder
+	fmt.Fprintf(&result, "[%d", s.data[0])
+	return result.String()
+}
+
+func main() {
+	var line, cmd string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	v := NewSet(0)
+	for scanner.Scan() {
+		fmt.Print("$")
+		line = scanner.Text()
+		fmt.Println(line)
+		parts := strings.Fields(line)
+		if len(parts) == 0 {
+			continue
+		}
+		cmd = parts[0]
+
+		switch cmd {
+		case "end":
+			return
+		case "init":
+			// value, _ := strconv.Atoi(parts[1])
+			// v = NewSet(value)
+		case "insert":
+			// for _, part := range parts[1:] {
+			// 	value, _ := strconv.Atoi(part)
+			// }
+		case "show":
+			fmt.Println(v.Show())
+		case "erase":
+			// value, _ := strconv.Atoi(parts[1])
+		case "contains":
+			// value, _ := strconv.Atoi(parts[1])
+		case "clear":
+		default:
+			fmt.Println("fail: comando invalido")
+		}
+	}
+}

57804749b9f3579cb816e876e7c0df3af507a3a8
Jean Oliveira <jean.oliveira2128@email.com>
2026-06-03 11:17:44 -0300

    Atividade setbuild, feita

--- ed/setbuild/src/go/main.go
+++ ed/setbuild/src/go/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -28,7 +29,7 @@ func (s *Set) Show() string {
 	var result strings.Builder
 	fmt.Fprintf(&result, "[%d", s.data[0])
 	for i := 1; i < s.size; i++ {
-		fmt.Fprintf(&result, " %d", s.data[i])
+		fmt.Fprintf(&result, ", %d", s.data[i])
 	}
 	result.WriteString("]")
 	return result.String()
@@ -40,19 +41,76 @@ func (s *Set) AcharPosicao(value int) (int, bool) {
 
 	for low <= high {
 		mid := low + (high-low)/2
-		if s.data
+		if s.data[mid] == value {
+			return mid, true
+		} else if s.data[mid] < value {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
 	}
+	return low, false
 }
 
 func (s *Set) insert(value int) {
+	idx, found := s.AcharPosicao(value)
 
+	if found {
+		return
+	}
+
+	if s.size == s.capacity {
+		newCap := 1
+		if s.capacity > 0 {
+			newCap = s.capacity * 2
+		}
+		s.Reserve(newCap)
+	}
+
+	for i := s.size; i > idx; i-- {
+		s.data[i] = s.data[i-1]
+	}
+
+	s.data[idx] = value
+	s.size++
+}
+
+func (s *Set) Reserve(newCapacity int) {
+	if newCapacity <= s.capacity {
+		return
+	}
+	newData := make([]int, newCapacity)
+	for i := 0; i < s.size; i++ {
+		newData[i] = s.data[i]
+	}
+	s.data = newData
+	s.capacity = newCapacity
+}
+
+func (s *Set) Contains(value int) bool {
+	_, found := s.AcharPosicao(value)
+	return found
+}
+
+func (s *Set) Erase(value int) bool {
+	idx, found := s.AcharPosicao(value)
+	if !found {
+		return false
+	}
+
+	for i := idx; i < s.size-1; i++ {
+		s.data[i] = s.data[i+1]
+	}
+
+	s.size--
+	return true
 }
 
 func main() {
 	var line, cmd string
 	scanner := bufio.NewScanner(os.Stdin)
 
-	v := NewSet(0)
+	s := NewSet(0)
 	for scanner.Scan() {
 		fmt.Print("$")
 		line = scanner.Text()
@@ -67,18 +125,27 @@ func main() {
 		case "end":
 			return
 		case "init":
-			// value, _ := strconv.Atoi(parts[1])
-			// v = NewSet(value)
+			value, _ := strconv.Atoi(parts[1])
+			s = NewSet(value)
 		case "insert":
-			// for _, part := range parts[1:] {
-			// 	value, _ := strconv.Atoi(part)
-			// }
+			for _, part := range parts[1:] {
+				value, _ := strconv.Atoi(part)
+				s.insert(value)
+			}
 		case "show":
-			fmt.Println(v.Show())
+			fmt.Println(s.Show())
 		case "erase":
-			// value, _ := strconv.Atoi(parts[1])
+			value, _ := strconv.Atoi(parts[1])
+			if !s.Erase(value) {
+				fmt.Println("value not found")
+			}
 		case "contains":
-			// value, _ := strconv.Atoi(parts[1])
+			value, _ := strconv.Atoi(parts[1])
+			if s.Contains(value) {
+				fmt.Println("true")
+			} else {
+				fmt.Println(false)
+			}
 		case "clear":
 		default:
 			fmt.Println("fail: comando invalido")

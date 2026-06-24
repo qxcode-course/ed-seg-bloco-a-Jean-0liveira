21bc3cb6cd39ca82687ab450281996491bd1d023
Jean Oliveira <jean.oliveira2128@email.com>
2026-06-24 16:51:20 -0300

    magicsearch, feita

---
+++ ed/magicsearch/src/go/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func MagicSearch(slice []int, value int) int {
+	inicio := 0
+	final := len(slice) - 1
+
+	for inicio <= final {
+		mid := inicio + (final-inicio)/2
+		if slice[mid] == value {
+			for mid+1 < len(slice) && slice[mid+1] == value {
+				mid++
+			}
+			return mid
+		} else if slice[mid] < value {
+			inicio = mid + 1
+		} else {
+			final = mid - 1
+		}
+	}
+	return inicio
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	parts := strings.Fields(scanner.Text())
+	slice := make([]int, 0, 1)
+	for _, elem := range parts[1 : len(parts)-1] {
+		value, _ := strconv.Atoi(elem)
+		slice = append(slice, value)
+	}
+
+	scanner.Scan()
+	value, _ := strconv.Atoi(scanner.Text())
+	result := MagicSearch(slice, value)
+	fmt.Println(result)
+}

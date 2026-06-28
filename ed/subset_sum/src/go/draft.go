ae5c13a0a7e86f4e0e77cba24284a589b02210cd
Jean Oliveira <jean.oliveira2128@email.com>
2026-06-28 18:47:17 -0300

    Fazendo subset_sum

--- ed/subset_sum/src/go/draft.go
+++ ed/subset_sum/src/go/draft.go
@@ -1,5 +1,37 @@
 package main
-import "fmt"
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Soma() bool {
+	return false
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if !scanner.Scan() {
+		return
+	}
+
+	line := strings.Fields(scanner.Text())
+
+	if !scanner.Scan() {
+		return
+	}
+
+	secondLine := strings.Fields(scanner.Text())
+
+	n, _ := strconv.Atoi(line[0])
+	v := make([]int, n)
+	for i := 0; i < n; i++ {
+		v[i], _ = strconv.Atoi(secondLine[i])
+	}
+
+	fmt.Println(Soma())
 }

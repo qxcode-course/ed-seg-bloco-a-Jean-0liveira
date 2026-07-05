f34a664e2b4e06bc4e41b9580087af4de89a0ca7
Jean Oliveira <jean.oliveira2128@email.com>
2026-07-05 09:13:04 -0300

    subset_sum feito

--- ed/subset_sum/src/go/draft.go
+++ ed/subset_sum/src/go/draft.go
@@ -8,7 +8,24 @@ import (
 	"strings"
 )
 
-func Soma() bool {
+func Soma(index int, somaAtual int, alvo int, vSoma []int) bool {
+
+	if somaAtual == alvo {
+		return true
+	}
+
+	if somaAtual > alvo || index >= len(vSoma) {
+		return false
+	}
+
+	if Soma(index+1, somaAtual, alvo, vSoma) {
+		return true
+	}
+
+	if Soma(index+1, somaAtual+vSoma[index], alvo, vSoma) {
+		return true
+	}
+
 	return false
 }
 
@@ -28,10 +45,16 @@ func main() {
 	secondLine := strings.Fields(scanner.Text())
 
 	n, _ := strconv.Atoi(line[0])
+	alvo, _ := strconv.Atoi(line[1])
+
 	v := make([]int, n)
 	for i := 0; i < n; i++ {
 		v[i], _ = strconv.Atoi(secondLine[i])
 	}
 
-	fmt.Println(Soma())
+	if Soma(0, 0, alvo, v) {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
 }

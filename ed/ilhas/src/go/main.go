2cea05d915e21c89704a75218e93e489298ae38a
Jean Oliveira <jean.oliveira2128@email.com>
2026-07-09 17:53:19 -0300

    ilhas, feito

--- ed/ilhas/src/go/main.go
+++ ed/ilhas/src/go/main.go
@@ -6,12 +6,39 @@ import (
 	"os"
 )
 
+func busca(grid [][]byte, l int, c int) {
+	if l < 0 || l >= len(grid) || c < 0 || c >= len(grid[0]) || grid[l][c] == '0' {
+		return
+	}
+
+	grid[l][c] = '0'
+
+	busca(grid, l+1, c)
+	busca(grid, l-1, c)
+	busca(grid, l, c+1)
+	busca(grid, l, c-1)
+}
+
 // Não modifique a assinatura da função numIslands
 // Ela é a função que será chamada no LeetCode para resolver o problema
 func numIslands(grid [][]byte) int {
-	//
-	_ := grid
-	return 0
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return 0
+	}
+
+	count := 0
+	nl := len(grid)
+	nc := len(grid[0])
+
+	for i := 0; i < nl; i++ {
+		for j := 0; j < nc; j++ {
+			if grid[i][j] == '1' {
+				count++
+				busca(grid, i, j)
+			}
+		}
+	}
+	return count
 }
 
 // Não modifique a função main

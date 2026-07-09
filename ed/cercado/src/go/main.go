a57ca81b0e72c3b42e21883a5a943e4fd15da278
Jean Oliveira <jean.oliveira2128@email.com>
2026-07-09 18:12:34 -0300

    Fazendo cercado

--- ed/cercado/src/go/main.go
+++ ed/cercado/src/go/main.go
@@ -6,9 +6,28 @@ import (
 	"os"
 )
 
+func busca(board [][]byte, l int, c int) {
+	if l < 0 || c < 0 || l >= len(board) || c >= len(board[0]) || board[l][c] != 'O' {
+		return
+	}
+
+	board[l][c] = '#'
+
+	busca(board, l+1, c)
+	busca(board, l-1, c)
+	busca(board, l, c+1)
+	busca(board, l, c-1)
+}
+
 // NÃO ALTERE A ASSINATURA DA FUNÇÃO solve
 func solve(board [][]byte) {
-	_ = board
+	if len(board) == 0 || len(board[0]) == 0 {
+		return
+	}
+
+	nl := len(board)
+	nc := len(board[0])
+
 }
 
 // NÃO ALTERE A MAIN

e46911fad5a551b325872d8c9140bc7e9f75c2db
Jean Oliveira <jean.oliveira2128@email.com>
2026-07-10 15:32:50 -0300

    cercado, feito

--- ed/cercado/src/go/main.go
+++ ed/cercado/src/go/main.go
@@ -28,6 +28,34 @@ func solve(board [][]byte) {
 	nl := len(board)
 	nc := len(board[0])
 
+	for i := 0; i < nl; i++ {
+		if board[i][0] == 'O' {
+			busca(board, i, 0)
+		}
+		if board[i][nc-1] == 'O' {
+			busca(board, i, nc-1)
+		}
+	}
+
+	for j := 0; j < nc; j++ {
+		if board[0][j] == 'O' {
+			busca(board, 0, j)
+		}
+		if board[nl-1][j] == 'O' {
+			busca(board, nl-1, j)
+		}
+	}
+
+	for i := 0; i < nl; i++ {
+		for j := 0; j < nc; j++ {
+			if board[i][j] == 'O' {
+				board[i][j] = 'X'
+			} else if board[i][j] == '#' {
+				board[i][j] = 'O'
+			}
+		}
+	}
+
 }
 
 // NÃO ALTERE A MAIN
